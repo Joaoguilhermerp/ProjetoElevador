88701d728bbf43dd0152c2d5898f84431c80ecf6
joaoguilhermerp <jaumdalolo@gmail.com>
2022-01-05 18:56:31 -0300

    Postagem oficial

---
+++ ProjetoFinalElevador/Model/Elevador.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinalElevador.Model
+{
+    internal class Elevador
+    {
+
+        public int AndarAtual { get; set; }
+        public int LimiteAndares { get; set; }
+        public int LimitePessoas { get; set; }
+        public int Pessoas { get; set; }
+
+
+
+        public void Inicializar(int limitePessoas, int limiteAndares)
+        {
+            LimiteAndares = limiteAndares;
+            LimitePessoas = limitePessoas;
+            AndarAtual = 0;
+            Pessoas = 0;
+
+        
+        }
+
+        public void Entrar()
+        {
+            if (Pessoas < LimitePessoas)
+            {
+                Pessoas++;
+                Console.WriteLine("Passageiros no elevador: " + Pessoas);
+            }
+            else
+            {
+                Console.WriteLine("O Elevador está lotado no momento, aguarde o retorno.");
+            }
+
+
+        }
+
+        public void Sair()
+        {
+            if (Pessoas > 0)
+            {
+                Pessoas--;
+                Console.WriteLine("Passageiros no elevador: " + Pessoas);
+            }
+            else
+            {
+                Console.WriteLine("O elevador está vazio");
+            }
+
+
+
+
+        }
+
+        public void Subir()
+        {
+            if (AndarAtual < LimiteAndares)
+            {
+                AndarAtual++;
+                Console.WriteLine(AndarAtual + "ºAndar");
+            }
+            else
+            {
+                Console.WriteLine("Você está no último andar");
+            }
+
+        }
+
+        public void Descer()
+        {
+            if (AndarAtual > 0)
+            {
+                AndarAtual--;
+                Console.WriteLine(AndarAtual + "ºAndar");
+            }
+            else
+            {
+                Console.WriteLine("Você já está no térreo");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ ProjetoFinalElevador/Program.cs
@@ -0,0 +1,107 @@
+﻿using ProjetoFinalElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevador elevador = new Elevador();
+
+
+            Inicializar(elevador);
+            ExibeMenu(elevador);
+        }
+
+        static void Inicializar(Elevador elevador)
+        {
+            int confirma = 0;
+
+            Console.WriteLine("Parabens voce acaba de adquirir seu elevador instantaneo patenteado pela ElevaDev");
+            Console.WriteLine("Para instalação precisamos dos dados iniciais:");
+                     
+            do
+            {
+                Console.WriteLine("Qual o limite de pessoas no elevador ? ");
+                int capacidade = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Qual o tamanho do predio em andares?");
+                int totalAndar = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine(@$"A configuração digitada foi: 
+                                 {capacidade} Pessoas e {totalAndar} Andares.");
+                Console.WriteLine("Gravar configuração?");
+                Console.WriteLine("1 - SIM \n0 - NÃO");
+                confirma = Convert.ToInt16(Console.ReadLine());
+
+                if (confirma == 1)
+                {
+                    elevador.Inicializar(capacidade, totalAndar);
+
+                    Console.Clear();
+                }
+            } while (confirma == 0);
+        }
+
+
+        static void ExibeMenu(Elevador elevador)
+            {
+                int confirma = 0;
+                int opcao;
+                do
+                {
+                    Console.WriteLine("\n\nO que você deseja fazer?");
+
+                    Console.WriteLine(@"Escolha uma opção:
+                                1 - Entrar
+                                2 - Sair
+                                3 - Subir
+                                4 - Descer
+                                5 - Desligar
+                                ");
+                    opcao = Convert.ToInt16(Console.ReadLine());
+
+
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            {
+                                Console.Clear();
+                                elevador.Entrar();
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.Clear();
+                                elevador.Sair();
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.Clear();
+                                elevador.Subir();
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.Clear();
+                                elevador.Descer();
+                                break;
+                            }
+                        case 5:
+                            {
+
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Digite uma opção valida");
+                                break;
+                            }
+                    }
+                } while (confirma == 0);
+            }
+        }
+    }
